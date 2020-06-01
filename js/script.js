b8ff1651eb338253f3e70f0bc07e9f2b075358b2
Kiselev Peter <kiselevpetr3@gmail.com>
2020-06-01 11:28:44 +0300

    SVG-спрайты

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function toggleMenu(e) {
+    e.preventDefault();
+    document.body.classList.toggle('menu-opened');
+}
+
+// var header   = document.querySelector('header');
+// var headroom = new Headroom(header, {
+//     tolerance: 5,
+// });
+// headroom.init();
+
